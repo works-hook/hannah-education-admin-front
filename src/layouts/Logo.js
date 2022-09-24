06cc6665497fae31fd1bfdb4eead9570a1f55ec4
Hannah <ghdcoalss33@gmail.com>
2022-09-24 16:35:35 +0900

    feat(logo): logo, change active menu color

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -1,12 +1,15 @@
-import { ReactComponent as LogoDark } from "../assets/images/logos/adminpro.svg";
+import { ReactComponent as LogoDark } from "../assets/images/logos/chalkboard-teacher-fill-svgrepo-com.svg";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
-  return (
-    <Link to="/">
-      <LogoDark />
-    </Link>
-  );
+  return ( <>
+      <div className="d-flex" style={{"color": "#444"}}>
+          <Link to="/">
+              <LogoDark />
+          </Link>
+          <h4 className="mt-sm-1 mx-sm-2"><strong>ADMIN</strong> Teacher</h4>
+      </div>
+    </> );
 };
 
 export default Logo;
