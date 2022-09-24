608fd1e7898a8c4d3a67b09425c6cda911503f90
Hannah <ghdcoalss33@gmail.com>
2022-09-24 14:45:57 +0900

    feat(sidebar): ã불필요한 메뉴, 라우터 삭제

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -13,54 +13,9 @@ const navigation = [
     icon: "bi bi-people"
   },
   {
-    title: "Dashboard",
-    href: "/starter",
-    icon: "bi bi-speedometer2",
-  },
-  {
-    title: "Alert",
-    href: "/alerts",
-    icon: "bi bi-bell",
-  },
-  {
-    title: "Badges",
-    href: "/badges",
-    icon: "bi bi-patch-check",
-  },
-  {
-    title: "Buttons",
-    href: "/buttons",
-    icon: "bi bi-hdd-stack",
-  },
-  {
     title: "Cards",
     href: "/cards",
     icon: "bi bi-card-text",
-  },
-  {
-    title: "Grid",
-    href: "/grid",
-    icon: "bi bi-columns",
-  },
-  {
-    title: "Table",
-    href: "/table",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Forms",
-    href: "/forms",
-    icon: "bi bi-textarea-resize",
-  },
-  {
-    title: "Breadcrumbs",
-    href: "/breadcrumbs",
-    icon: "bi bi-link",
-  },
-  {
-    title: "About",
-    href: "/about",
-    icon: "bi bi-people",
   },
 ];
 
