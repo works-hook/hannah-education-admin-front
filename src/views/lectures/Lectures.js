547a1e6dcb36495a837620c9460129290c92abad
Hannah <ghdcoalss33@gmail.com>
2022-09-29 14:15:38 +0900

    feat(lectures): modify list page at large and add list footer

--- src/views/lectures/Lectures.js
+++ src/views/lectures/Lectures.js
@@ -1,72 +1,119 @@
-import { Card, CardBody, CardTitle, Col, Container, Row, Table } from "reactstrap";
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    Col,
+    Container,
+    Row,
+    Table,
+    Button
+} from "reactstrap";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import user1 from "../../assets/images/users/user1.jpg";
-import user2 from "../../assets/images/users/user2.jpg";
-import user3 from "../../assets/images/users/user3.jpg";
-import user4 from "../../assets/images/users/user4.jpg";
-import user5 from "../../assets/images/users/user5.jpg";
+import { useState } from "react";
+import ListFooter from "../utils/ListFooter";
 
 const tableData = [
     {
         id: 1,
-        avatar: user1,
-        name: "Hanna Gover",
-        project: "Flexy React",
+        name: "Java 기본 문법",
         status: "danger",
         startDate: "2022-01-03",
         endDate: "2022-06-30",
     },
     {
         id: 2,
-        avatar: user2,
-        name: "Hanna Gover",
-        project: "Lading pro React",
+        name: "Java 객체 지향",
         status: "success",
         startDate: "2022-01-03",
         endDate: "2022-06-30",
     },
     {
         id: 3,
-        avatar: user3,
-        name: "Hanna Gover",
-        project: "Elite React",
+        name: "DataBase (MySQL)",
         status: "warning",
         startDate: "2022-01-03",
         endDate: "2022-06-30",
     },
     {
         id: 4,
-        avatar: user4,
-        name: "Hanna Gover",
-        project: "Flexy React",
+        name: "Git / GitHub",
         status: "danger",
         startDate: "2022-01-03",
         endDate: "2022-06-30",
     },
     {
         id: 5,
-        avatar: user5,
-        name: "Hanna Gover",
-        project: "Ample React",
+        name: "Spring Framework",
         status: "success",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 6,
+        name: "Spring Framework",
+        status: "danger",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 7,
+        name: "Spring Framework",
+        status: "warning",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 8,
+        name: "Spring Framework",
+        status: "success",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 9,
+        name: "Spring Framework",
+        status: "danger",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 10,
+        name: "Spring Framework",
+        status: "success",
+        startDate: "2022-01-03",
+        endDate: "2022-06-30",
+    },
+    {
+        id: 11,
+        name: "Spring Framework",
+        status: "danger",
         startDate: "2022-01-03",
         endDate: "2022-06-30",
     },
 ];
 
 const Lectures = () => {
+    const [search, setSearch] = useState("");
+    const onSearchChange = (e) => setSearch(e.target.value);
+
+    const searchData = tableData.filter((data) => {
+        return data.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    });
+
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * 5;
+
     return (<div>
         <Card>
             <CardBody>
                 <Container>
-                    <Row className="d-flex">
+                    <Row>
                         <Col className="p-2 w-100">
                             <CardTitle tag="h3">{ "강의 리스트" }</CardTitle>
                         </Col>
                         <Col className="d-flex flex-row-reverse align-items-center">
                             <Link to="/lecture">
-                                <Button variant="secondary">
+                                <Button color="secondary">
                                     강의 등록
                                 </Button>
                             </Link>
@@ -77,41 +124,25 @@ const Lectures = () => {
                 <Table className="no-wrap mt-3 align-middle" responsive borderless>
                     <thead>
                     <tr>
-                        <th>No.</th>
+                        <th className="text-center">No.</th>
                         <th>제목</th>
-                        <th>설명</th>
                         <th>Period</th>
                         <th>Status</th>
                         <th></th>
                     </tr>
                     </thead>
                     <tbody>
-                    { tableData.map((tdata, index) => (
-                        <tr key={ index } className="border-top">
-                            <td>{ tdata.id }</td>
-                            <td>
-                                <div className="d-flex align-items-center p-2">
-                                    <img
-                                        src={ tdata.avatar }
-                                        className="rounded-circle"
-                                        alt="avatar"
-                                        width="45"
-                                        height="45"
-                                    />
-                                    <div className="ms-3">
-                                        <h6 className="mb-0">{ tdata.name }</h6>
-                                        <span className="text-muted">{ tdata.email }</span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>{ tdata.project }</td>
+                    { searchData.slice(offset, offset + 5).map((tdata, index) => (
+                        <tr key={ index } className="border-top list-td">
+                            <td className="text-center">{ tdata.id }</td>
+                            <td>{ tdata.name }</td>
                             <td>{ tdata.startDate + " ~ " + tdata.endDate }</td>
                             <td>
                                 <span className={`p-2 bg-${ tdata.status } rounded-circle d-inline-block ms-3`}></span>
                             </td>
-                            <td>
+                            <td className="text-center">
                                 <Link to={`/lecture/${ tdata.id }`}>
-                                    <Button variant="outline-primary">
+                                    <Button color="outline-primary">
                                         상세 보기
                                     </Button>
                                 </Link>
@@ -120,6 +151,13 @@ const Lectures = () => {
                     ))}
                     </tbody>
                 </Table>
+                <ListFooter
+                    search={ search }
+                    onSearchChange={ onSearchChange }
+                    total={ searchData.length }
+                    page={ page }
+                    setPage={ setPage }
+                />
             </CardBody>
         </Card>
     </div>);
